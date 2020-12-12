0e594dd2960a8f58a2f29be6e2dd71f71f510cbc
irtaxahaider <75080903+irtaxahaider@users.noreply.github.com>
2020-12-12 18:51:18 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+
+public class Main{
+    public static void main(String[] args) {
+   for (int i = 0; i <= 5; i++) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+    for (int i = 5; i >= 1; --i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+    // kajsdfkdafhsdkaljfhsdjklfhsdkaljfhjsdkfhkjsalhfsdkjlfhkj
+    for (int i = 0; i <= 5; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    
+    for (int i = 5; i >= 1; --i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    }
+}
